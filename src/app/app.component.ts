2ce40e3349f8cb8d724251b4e5c1c2ea63d81bb5
David Martin <davidrenzzo@gmail.com>
2024-04-29 11:50:43 -0500

    fix: update app.component with layout & list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { CardDocComponent } from './components/card-doc/card-doc.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    CardDocComponent
+    CardDocComponent,
+    LayoutPageComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
