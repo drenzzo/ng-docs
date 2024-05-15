de9a3490262f75fe19fe9fd61660ac5589e948e6
David [U22] <davidsalazar@multiplica.com>
2024-05-15 09:52:31 -0500

    feat: input search

---
+++ src/app/components/search-doc/search-doc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'docs-search-doc',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-doc.component.html',
+  styles: ``
+})
+export class SearchDocComponent {
+
+}
