69755be3dfaf23ca1efb1a1f2b73d3c50e1fdfc4
David [U22] <davidsalazar@multiplica.com>
2024-05-15 09:53:13 -0500

    feat: input search in page layout

--- src/app/pages/layout-page/layout-page.component.ts
+++ src/app/pages/layout-page/layout-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ListPageComponent } from '../list-page/list-page.component';
+import { SearchDocComponent } from '../../components/search-doc/search-doc.component';
 
 @Component({
   selector: 'docs-layout-page',
   standalone: true,
   imports: [
-    ListPageComponent
+    ListPageComponent,
+    SearchDocComponent
   ],
   templateUrl: './layout-page.component.html',
   styles: ``
