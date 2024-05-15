6365f1b0eacb44a5e783e846887f3ca3529d460f
David [W11] <davidrenzzo@gmail.com>
2024-05-15 11:19:12 -0500

    feat: get items with service and show in datalist

--- src/app/components/search-doc/search-doc.component.ts
+++ src/app/components/search-doc/search-doc.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Doc } from '../../interfaces/doc.interface';
+import { DocsService } from '../../services/doc.service';
+
 
 @Component({
   selector: 'docs-search-doc',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './search-doc.component.html',
   styles: ``
 })
 export class SearchDocComponent {
+
+  public items: Doc[] = [];
+
+  constructor(
+    private docsService: DocsService
+  ) { }
+
+  ngOnInit(): void {
+    this.docsService.getDocs()
+      .subscribe((data: Doc[]) => {
+        this.items = data;
+      });
+  }
+
 
 }
